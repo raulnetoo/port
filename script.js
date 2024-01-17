d148a7348e43c061708b7d8ca0242929991db0e5
raulnetoo <raulalvarezneto@gmail.com>
2024-01-17 17:04:21 -0300

    prod

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-// script.js
-
 function trocarImagem(element, novaImagem) {
-    var imagemProduto = element.querySelector('img');
-    var imagemAtual = imagemProduto.src;
-  
-    // Verifica se a imagem atual é a imagem original
-    if (imagemAtual.includes('produto1.png')) {
+  var imagemProduto = element.querySelector('img');
+  var imagemAtual = imagemProduto.src;
+  var imagemOriginal = imagemProduto.dataset.original;
+
+  // Verifica se a imagem atual é a imagem original
+  if (imagemAtual.includes(imagemOriginal)) {
       // Se for, troca para a nova imagem
       imagemProduto.src = 'img/prod/' + novaImagem;
-    } else {
+  } else {
       // Se não for, volta para a imagem original
-      imagemProduto.src = 'img/prod/produto1.png';
-    }
+      imagemProduto.src = 'img/prod/' + imagemOriginal;
   }
-  
+}
